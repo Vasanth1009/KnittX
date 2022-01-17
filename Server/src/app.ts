c52b47874ed5c72b46cb9328a50b38a3f5a51e45
Vasanth <vasanthprabhakaran1999@gmail.com>
2022-01-17 21:17:13 +0530

    Update cors header

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -5,21 +5,7 @@ import customerRoute from './routes/customerRoute';
 const app = express();
 
 app.use(express.json());
-// app.use(cors({ origin : '*' }));
-app.use(function (req: Request, res: Response, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-  next();
-});
+app.use(cors({ origin: '*' }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to KnittX-API');
