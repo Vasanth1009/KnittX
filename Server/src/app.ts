f0bc89a5bad644220c0c3de63f6644508b53997e
Vasanth <vasanthprabhakaran1999@gmail.com>
2022-01-17 14:36:28 +0530

    Updated vercel json file

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,11 +1,15 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import customerRoute from './routes/customerRoute';
 
-const app = express();
+const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to KnittX-API');
+});
 
 app.use('/api/customer', customerRoute);
 
