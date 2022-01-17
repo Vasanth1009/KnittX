ce64c6d9fcdf7a3e63b0b366fb9f0ff55fd23dc2
Vasanth Prabhakaran <55186278+Vasanth1009@users.noreply.github.com>
2022-01-17 19:23:42 +0530

    Update app.ts

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -5,7 +5,7 @@ import customerRoute from './routes/customerRoute';
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to KnittX-API');
